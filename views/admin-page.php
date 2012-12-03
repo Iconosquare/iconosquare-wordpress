83843c67564de293521faa6c7623d52213c3da0e
gaets86 <gaetan.houssin@gmail.com>
2012-12-03 14:24:42 +0100

    Database creation

--- views/admin-page.php
+++ views/admin-page.php
@@ -10,7 +10,8 @@
                 </div>
                 <div class="widget-holder">
                     <div class="content-column">
-                        <form action="post" action="">
+                        <form method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+                            <input type="hidden" name="settingPlugin" value='1'>
                             <input type="hidden" id="notrack" value='1'>
                             <table class="form-table">
                                 <tbody>
@@ -18,36 +19,36 @@
                                         <th scope="row"><label for="choose-content">Content</label></th>
                                         <td>
                                         <select name="choose-content" id="choose-content">
-                                            <option selected="selected" value="myfeed">My Feed</option>
-                                            <option value="hashtag">Hashtag</option>
+                                            <option <?php if($pluginValues->content == 'myfeed') echo 'selected="selected"'; ?> value="myfeed">My Feed</option>
+                                            <option <?php if($pluginValues->content == 'hashtag') echo 'selected="selected"'; ?> value="hashtag">Hashtag</option>
                                         </select>
                                         </td>
                                     </tr>
 
                                     <tr valign="top" class="user-group">
                                         <th scope="row"><label for="username">Username</label></th>
-                                        <td><input name="username" type="text" id="username" value="statigram" class="regular-text"></td>
+                                        <td><input name="username" type="text" id="username" value="<?php echo $pluginValues->username; ?>" class="regular-text"></td>
                                     </tr>
 
                                     <tr valign="top" class="user-group">
                                         <th scope="row">Informations</th>
                                         <td> <fieldset><legend class="screen-reader-text"><span>Show infos</span></legend><label for="infos">
-                                        <input name="infos" type="checkbox" id="infos" checked="checked">
+                                        <input name="infos" type="checkbox" id="infos" <?php if($pluginValues->infos) echo 'checked="checked"'; ?>>
                                         Show infos</label>
                                         </fieldset></td>
                                     </tr>
 
                                     <tr valign="top" class="hash-group">
                                         <th scope="row"><label for="hashtag">Hashtag</label></th>
-                                        <td><input name="hashtag" type="text" id="hashtag" value="statigram" class="regular-text"></td>
+                                        <td><input name="hashtag" type="text" id="hashtag" value="<?php echo $pluginValues->hashtag; ?>" class="regular-text"></td>
                                     </tr>
 
                                     <tr valign="top">
                                         <th scope="row"><label for="linking">Linking to</label></th>
                                         <td>
                                         <select name="linking" id="linking">
-                                            <option selected="selected" value="statigram">Statigram</option>
-                                            <option value="instagram">Instagram</option>
+                                            <option <?php if($pluginValues->link == 'statigram') echo 'selected="selected"'; ?> value="statigram">Statigram</option>
+                                            <option <?php if($pluginValues->link == 'instagram') echo 'selected="selected"'; ?> value="instagram">Instagram</option>
                                         </select>
                                         &nbsp; Choose where the link redirect
                                         </td>
@@ -57,20 +58,20 @@
 
                                     <tr valign="top">
                                         <th scope="row"><label for="width">Width (in pixels)</label></th>
-                                        <td><input name="width" type="text" id="width" value="380" class="regular-text"></td>
+                                        <td><input name="width" type="text" id="width" value="<?php echo $pluginValues->width; ?>" class="regular-text"></td>
                                     </tr>
 
                                     <tr valign="top">
                                         <th scope="row"><label for="height">Height (in pixels)</label></th>
-                                        <td><input name="height" type="text" id="height" value="420" class="regular-text"></td>
+                                        <td><input name="height" type="text" id="height" value="<?php echo $pluginValues->height; ?>" class="regular-text"></td>
                                     </tr>
 
                                     <tr valign="top">
                                         <th scope="row"><label for="choose-mode">Mode</label></th>
                                         <td>
                                         <select name="choose-mode" id="choose-mode">
-                                            <option selected="selected" value="grid">Grid</option>
-                                            <option value="slideshow">Slideshow</option>
+                                            <option <?php if($pluginValues->mode == 'grid') echo 'selected="selected"'; ?> value="grid">Grid</option>
+                                            <option <?php if($pluginValues->link == 'slideshow') echo 'selected="selected"'; ?> value="slideshow">Slideshow</option>
                                         </select>
                                         </td>
                                     </tr>
@@ -79,24 +80,24 @@
                                         <th scope="row"><label>Layout</label></th>
                                         <td>
                                             <select name="layoutX" id="layoutX">
-                                                <option value="1">1</option>
-                                                <option value="2">2</option>
-                                                <option value="3" selected="">3</option>
-                                                <option value="4">4</option>
-                                                <option value="5">5</option>
-                                                <option value="6">6</option>
-                                                <option value="7">7</option>
+                                                <option <?php if($pluginValues->layoutX == '1') echo 'selected="selected"'; ?> value="1">1</option>
+                                                <option <?php if($pluginValues->layoutX == '2') echo 'selected="selected"'; ?> value="2">2</option>
+                                                <option <?php if($pluginValues->layoutX == '3') echo 'selected="selected"'; ?> value="3" selected="">3</option>
+                                                <option <?php if($pluginValues->layoutX == '4') echo 'selected="selected"'; ?> value="4">4</option>
+                                                <option <?php if($pluginValues->layoutX == '5') echo 'selected="selected"'; ?> value="5">5</option>
+                                                <option <?php if($pluginValues->layoutX == '6') echo 'selected="selected"'; ?> value="6">6</option>
+                                                <option <?php if($pluginValues->layoutX == '7') echo 'selected="selected"'; ?> value="7">7</option>
                                             </select>
                                             <span class="sep">X</span>
                                             <select name="layoutY" id="layoutY">
-                                                <option value="1">1</option>
-                                                <option value="2" selected="">2</option>
-                                                <option value="3">3</option>
-                                                <option value="4">4</option>
-                                                <option value="5">5</option>
-                                                <option value="6">6</option>
-                                                <option value="7">7</option>
-                                                <option value="8">8</option>
+                                                <option <?php if($pluginValues->layoutY == '1') echo 'selected="selected"'; ?> value="1">1</option>
+                                                <option <?php if($pluginValues->layoutY == '2') echo 'selected="selected"'; ?> value="2" selected="">2</option>
+                                                <option <?php if($pluginValues->layoutY == '3') echo 'selected="selected"'; ?> value="3">3</option>
+                                                <option <?php if($pluginValues->layoutY == '4') echo 'selected="selected"'; ?> value="4">4</option>
+                                                <option <?php if($pluginValues->layoutY == '5') echo 'selected="selected"'; ?> value="5">5</option>
+                                                <option <?php if($pluginValues->layoutY == '6') echo 'selected="selected"'; ?> value="6">6</option>
+                                                <option <?php if($pluginValues->layoutY == '7') echo 'selected="selected"'; ?> value="7">7</option>
+                                                <option <?php if($pluginValues->layoutY == '8') echo 'selected="selected"'; ?> value="8">8</option>
                                             </select> &nbsp;
                                             <span class="help-inline">Horizontal x Vertical</span>
                                         </td>
@@ -104,18 +105,18 @@
 
                                     <tr valign="top" class="mode-grid">
                                         <th scope="row"><label for="padding">Padding (in pixels)</label></th>
-                                        <td><input name="padding" type="text" id="padding" value="10" class="regular-text"></td>
+                                        <td><input name="padding" type="text" id="padding" value="<?php echo $pluginValues->padding; ?>" class="regular-text"></td>
                                     </tr>
 
                                     <tr valign="top" class="mode-slideshow">
                                         <th scope="row"><label for="pace">Transition between two pictures</label></th>
-                                        <td><input name="pace" type="text" id="pace" value="10" class="regular-text"></td>
+                                        <td><input name="pace" type="text" id="pace" value="<?php echo $pluginValues->pace; ?>" class="regular-text"></td>
                                     </tr>
 
                                     <tr valign="top">
                                         <th scope="row">Photo border</th>
                                         <td> <fieldset><legend class="screen-reader-text"><span>Border white with shadow</span></legend><label for="photo-border">
-                                        <input name="photo-border" type="checkbox" id="photo-border" checked="checked">
+                                        <input name="photo-border" type="checkbox" id="photo-border" <?php if($pluginValues->photoBorder) echo 'checked="checked"'; ?>>
                                         Border white with shadow</label>
                                         </fieldset></td>
                                     </tr>
@@ -124,20 +125,20 @@
 
                                     <tr valign="top" class="background">
                                         <th scope="row"><label for="background">Background color</label></th>
-                                        <td><input name="background" type="text" id="background" value="FFFFFF" class="regular-text color {required: false}"><br> Leave empty for transparent</td>
+                                        <td><input name="background" type="text" id="background" value="<?php echo $pluginValues->background; ?>" class="regular-text color {required: false}"><br> Leave empty for transparent</td>
                                     </tr>
 
                                     <tr valign="top" class="text">
                                         <th scope="row"><label for="text">Text color</label></th>
-                                        <td><input name="text" type="text" id="text" value="777777" class="regular-text color {required: false}"></td>
+                                        <td><input name="text" type="text" id="text" value="<?php echo $pluginValues->text; ?>" class="regular-text color {required: false}"></td>
                                     </tr>
 
                                     <tr valign="top" class="text">
                                         <th scope="row"><label for="text">Widget border</label></th>
                                         <td>
-                                            <input name="widget-border" type="checkbox" id="widget-border" checked="checked">
-                                            <input name="radius" type="radius" id="radius" value="5" class="small-text">px (radius)
-                                            <input name="border-color" type="text" id="border-color" value="DDDDDD" class="regular-text color {required: false}">
+                                            <input name="widget-border" type="checkbox" id="widget-border" <?php if($pluginValues->widgetBorder) echo 'checked="checked"'; ?>>
+                                            <input name="radius" type="radius" id="radius" value="<?php echo $pluginValues->radius; ?>" class="small-text">px (radius)
+                                            <input name="border-color" type="text" id="border-color" value="<?php echo $pluginValues->borderColor; ?>" class="regular-text color {required: false}">
                                         </td>
                                     </tr>
                                 </tbody>
